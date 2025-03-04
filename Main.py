fc651d7323bb7087e9b358ebf152ea33aff6856d
Lavox122 <Jinsheng22222@gmail.com>
2025-03-05 00:14:49 +0800

    Modified pics and coding
    changed the names of images and added coding

--- Main.py
+++ Main.py
@@ -1,5 +1,20 @@
 import pygame
 import sys
+import cv2
+
+
+#Level 1 coding
+#loading all the pictures
+#Player
+PlayerIdleIMG = cv2.imread('PythonAssignment\\Images\\Player\\Idle.png')
+PlayerBlockIMG = cv2.imread('PythonAssignment\\Images\\Player\\Block.png')
+PlayerPunchIMG = cv2.imread('PythonAssignment\\Image\\Player\\Punch.png')
+
+#Enemy
+EnemyIdleIMG = cv2.imread('PythonAssignment\\Images\\Enemy\\Level1\\Idle.png')
+EnemyBlockIMG = cv2.imread('PythonAssignment\\Images\\Enemy\\Level1\\Block.png')
+EnemyAttackWindUpIMG = cv2.imread('PythonAssignment\\Images\\Enemy\\Level1\\Attack_Wind-up.png')
+EnemyAttackIMG = cv2.imread('PythonAssignment\\Images\\Enemy\\Level1\\Attack.png')
 
 #initialize the window
 pygame.init()
@@ -25,6 +40,10 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+
+    cv2.imshow('Test', PlayerIdleIMG)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
     screen.fill((0, 0, 0))
     pygame.display.flip()
